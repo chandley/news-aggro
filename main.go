4597a8a46a9e80fd1f47a6be61611feb33fcbc49
Chris Handley <chrisrhandley@gmail.com>
2017-07-13 17:16:41 +0100

    introduce EIN feed, comment out development feeds

--- main.go
+++ main.go
@@ -17,13 +17,16 @@ func main() {
 	feedServer := NewFeed()
 
 	sources := []*RSSFetcher {
-		NewRSSFetcher("http://feeds.bbci.co.uk/news/rss.xml?edition=uk", "bbc news", ".story-body__inner", feedServer),
+		//NewRSSFetcher("http://feeds.bbci.co.uk/news/rss.xml?edition=uk", "bbc news", ".story-body__inner", feedServer),
 		NewRSSFetcher("http://www.londonstockexchange.com/exchange/CompanyNewsRSS.html?newsSource=RNS&indexSymbol=UKX", "LSE FTSE whatever", ".bg", feedServer),
 		NewRSSFetcher("http://www.topazworld.com/en/rss/news", "Topaz", "#content", feedServer),
-		NewRSSFetcher("https://twitrss.me/twitter_search_to_rss/?term=bieber", "Bieber News from Twitter", ".tweet-text", feedServer),
-		NewRSSFetcher("https://twitrss.me/twitter_user_to_rss/?user=quii", "Chris James News from Twitter", ".tweet-text",feedServer),
-		NewRSSFetcher("https://twitrss.me/twitter_user_to_rss/?user=chrisrhandley", "Chris Handley News from Twitter", ".tweet-text",feedServer),
-		NewRSSFetcher("http://lorem-rss.herokuapp.com/feed?unit=second&interval=10", "Lorem ipsum feed", ".none",feedServer),
+		//NewRSSFetcher("https://twitrss.me/twitter_search_to_rss/?term=bieber", "Bieber News from Twitter", ".tweet-text", feedServer),
+		//NewRSSFetcher("https://twitrss.me/twitter_user_to_rss/?user=quii", "Chris James News from Twitter", ".tweet-text",feedServer),
+		//NewRSSFetcher("https://twitrss.me/twitter_user_to_rss/?user=chrisrhandley", "Chris Handley News from Twitter", ".tweet-text",feedServer),
+		//NewRSSFetcher("http://lorem-rss.herokuapp.com/feed?unit=second&interval=10", "Lorem ipsum feed", ".none",feedServer),
+		//NewRSSFetcher("https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&type=10-k&company=&dateb=&owner=include&start=0&count=40&output=atom", "SEC 10k", ".none", feedServer),
+		//NewRSSFetcher("https://www.sec.gov/Archives/edgar/usgaap.rss.xml", "SEC gaap", ".none", feedServer),
+		NewRSSFetcher("https://investing.einnews.com/rss/5hMDxhc02nswfIlH", "EIN feed", ".none", feedServer),
 	}
 
 	for _, fetcher := range sources {
